44f999a4ea49c1cf32b611be64d70e9b5190ffe7
gesti9 <sultan.astana0@gmail.com>
2022-03-14 12:41:34 +0600

    Crud Modul

---
+++ src/test/java/kz/gesti9/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.gesti9.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
